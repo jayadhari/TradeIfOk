34f6c869519ae7e12e7a3baf7361b9f2eb5475c0
jayadhari <jayadhari101@yahoo.com>
2013-10-24 17:55:37 +0530

    Initial check in for Initial Site Creation task. Basic functionality except retrieving image urls from database is implemented.

---
+++ TradeIfOk/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TradeIfOk.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.UnderConstruction = "http://www.ict-romeo.eu/images/Under-Construction.gif";
+            ViewBag.MorningImage = "https://sites.google.com/a/tradeifok.com/resources/home/morning.PNG";
+            ViewBag.NoonImage = "https://sites.google.com/a/tradeifok.com/resources/home/noon.PNG";
+            ViewBag.EveningImage = "https://sites.google.com/a/tradeifok.com/resources/home/Evening.PNG";
+            ViewBag.NightImage = "https://sites.google.com/a/tradeifok.com/resources/home/night.PNG";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
